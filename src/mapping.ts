75a7b75e5545dee9d868426f5560f69b7b43bc6b
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-08-08 16:21:15 +0200

    wip

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,7 +4,7 @@ import {
   ProductsPaid as ProductsPaidEvent,
   TokenSliced as TokenSlicedEvent,
   TriggeredRelease as TriggeredReleaseEvent,
-  Unpaused as UnpausedEvent
+  Unpaused as UnpausedEvent,
 } from "../generated/Slice/Slice"
 import {
   OwnershipTransferred,
@@ -12,8 +12,9 @@ import {
   ProductsPaid,
   TokenSliced,
   TriggeredRelease,
-  Unpaused
+  Unpaused,
 } from "../generated/schema"
+import { log, Address, Bytes, ByteArray } from "@graphprotocol/graph-ts"
 
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent
@@ -38,7 +39,12 @@ export function handleProductsPaid(event: ProductsPaidEvent): void {
   let entity = new ProductsPaid(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   )
-  entity.slicerAddresses = event.params.slicerAddresses
+  // let bytesAddresses: Bytes[]
+  // let addrArray = event.params.slicerAddresses
+  // for (let i = 0; i < addrArray.length; i++) {
+  //   bytesAddresses[i] = addrArray[i]
+  // }
+  entity.slicerAddresses = event.params.slicerAddresses as Array<Bytes>
   entity.productIds = event.params.productIds
   entity.quantities = event.params.quantities
   entity.totalPaid = event.params.totalPaid
@@ -49,9 +55,15 @@ export function handleTokenSliced(event: TokenSlicedEvent): void {
   let entity = new TokenSliced(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   )
+  // let bytesAddresses = event.params.payees as Array<Bytes>
+  // let addrArray = event.params.payees
+  // for (let i = 0; i < addrArray.length; i++) {
+  //   bytesAddresses[i] = ByteArray.fromHexString(addrArray[i].toHexString())
+  // }
+
   entity.slicerAddress = event.params.slicerAddress
   entity.tokenId = event.params.tokenId
-  entity.payees = event.params.payees
+  entity.payees = event.params.payees as Array<Bytes>
   entity.shares = event.params.shares
   entity.save()
 }
