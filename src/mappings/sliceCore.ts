0c4285e6d41fb27b92f5ec8403aa498d998de5ac
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-03-28 01:38:03 +0200

    mappings

--- src/mappings/sliceCore.ts
+++ src/mappings/sliceCore.ts
@@ -1,32 +1,214 @@
-import { Payee, PayeeSlicer } from "../../generated/schema"
 import {
+  Slicer as SlicerEntity,
+  Payee,
+  PayeeSlicer,
+  CurrencySlicer,
+  Currency
+} from "../../generated/schema"
+import {
+  TokenSliced as TokenSlicedEvent,
+  TokenResliced as TokenReslicedEvent,
+  SlicerControllerSet as SlicerControllerSetEvent,
+  RoyaltySet as RoyaltySetEvent,
   TransferSingle as TransferSingleEvent,
   TransferBatch as TransferBatchEvent
 } from "../../generated/SliceCore/SliceCore"
-import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
+import {
+  Address,
+  BigInt,
+  Bytes,
+  DataSourceContext
+} from "@graphprotocol/graph-ts"
+import { Slicer } from "../../generated/templates"
+
+export function handleTokenSliced(event: TokenSlicedEvent): void {
+  let slicerAddress = event.params.slicerAddress
+  let slicerId = event.params.tokenId.toHex()
+  let payees = event.params.payees
+  let minimumShares = event.params.minimumShares
+  let currencies = event.params.currencies
+  let releaseTimelock = event.params.releaseTimelock
+  let transferableTimelock = event.params.transferableTimelock
+  let isImmutable = event.params.isImmutable
+  let isControlled = event.params.isControlled
+  let slicerVersion = event.params.slicerVersion
+  let creator = event.transaction.from.toHexString()
+  let totalSlices: BigInt
+
+  let slicer = new SlicerEntity(slicerId)
+
+  slicer.slicerVersion = slicerVersion
+  slicer.address = slicerAddress
+  slicer.minimumSlices = minimumShares
+  slicer.createdAtTimestamp = event.block.timestamp
+  slicer.releaseTimelock = releaseTimelock
+  slicer.transferableTimelock = transferableTimelock
+  slicer.isImmutable = isImmutable
+  slicer.creator = creator
+  slicer.protocolFee = BigInt.fromI32(25)
+  slicer.royaltyPercentage = BigInt.fromI32(50)
+  slicer.royaltyReceiver = creator
+
+  if (isControlled) {
+    slicer.controller = creator
+  }
+
+  for (let i = 0; i < currencies.length; i++) {
+    let currencyAddress = currencies[i].toHexString()
+
+    let currency = Currency.load(currencyAddress)
+    if (!currency) {
+      currency = new Currency(currencyAddress)
+      currency.save()
+    }
+
+    let currencySlicer = new CurrencySlicer(currencyAddress + "-" + slicerId)
+    currencySlicer.currency = currencyAddress
+    currencySlicer.slicer = slicerId
+    currencySlicer.save()
+  }
+
+  for (let i = 0; i < payees.length; i++) {
+    let payeeAddress = payees[i].account.toHexString()
+    let share = payees[i].shares
+
+    let payee = Payee.load(payeeAddress)
+    if (!payee) {
+      payee = new Payee(payeeAddress)
+      payee.save()
+    }
+
+    let payeeSlicer = new PayeeSlicer(payeeAddress + "-" + slicerId)
+    payeeSlicer.payee = payeeAddress
+    payeeSlicer.slicer = slicerId
+    payeeSlicer.slices = share
+    payeeSlicer.save()
+
+    totalSlices = totalSlices.plus(share)
+  }
+
+  let creatorPayee = Payee.load(creator)
+  if (!creatorPayee) {
+    creatorPayee = new Payee(creator)
+    creatorPayee.save()
+  }
+
+  slicer.slices = totalSlices
+  slicer.save()
+
+  let context = new DataSourceContext()
+  context.setString("slicerId", slicerId)
+  Slicer.createWithContext(event.params.slicerAddress, context)
+}
+
+export function handleTokenResliced(event: TokenReslicedEvent): void {
+  let slicerId = event.params.tokenId.toHex()
+  let accounts = event.params.accounts
+  let tokensDiffs = event.params.tokensDiffs
+  let slicer = SlicerEntity.load(slicerId)!
+  let totalSlices = slicer.slices
+
+  for (let i = 0; i < accounts.length; i++) {
+    let account = accounts[i].toHexString()
+    let tokenDiff = BigInt.fromI32(tokensDiffs[i])
+
+    let payee = Payee.load(account)
+    if (!payee) {
+      payee = new Payee(account)
+      payee.save()
+    }
+
+    let payeeSlicer = PayeeSlicer.load(account + "-" + slicerId)
+    if (!payeeSlicer) {
+      payeeSlicer = new PayeeSlicer(account + "-" + slicerId)
+      payeeSlicer.payee = account
+      payeeSlicer.slicer = slicerId
+    }
+    payeeSlicer.slices = payeeSlicer.slices.plus(tokenDiff)
+    payeeSlicer.save()
+
+    totalSlices = totalSlices.plus(tokenDiff)
+  }
+
+  // TODO: Check operations with int work as expected
+
+  slicer.slices = totalSlices
+  slicer.save()
+}
+
+export function handleSlicerControllerSet(
+  event: SlicerControllerSetEvent
+): void {
+  let slicerId = event.params.tokenId.toHex()
+  let slicerController = event.params.slicerController
+  let controller = slicerController.toHexString()
+  let slicer = SlicerEntity.load(slicerId)!
+
+  let payee = Payee.load(controller)
+  if (!payee) {
+    payee = new Payee(controller)
+    payee.save()
+  }
+
+  slicer.controller = controller
+  slicer.save()
+}
+
+export function handleRoyaltySet(event: RoyaltySetEvent): void {
+  let slicerId = event.params.tokenId.toHex()
+  let isSlicer = event.params.isSlicer
+  let isActive = event.params.isActive
+  let royaltyPercentage = event.params.royaltyPercentage
+  let slicer = SlicerEntity.load(slicerId)!
+
+  if (isActive) {
+    slicer.royaltyPercentage = royaltyPercentage
+  } else {
+    slicer.royaltyPercentage = BigInt.fromI32(50)
+  }
+
+  if (isSlicer) {
+    let slicerAddress = slicer.address.toHexString()
+    let payee = Payee.load(slicerAddress)
+    if (!payee) {
+      payee = new Payee(slicerAddress)
+      payee.save()
+    }
+
+    slicer.royaltyReceiver = slicer.address.toHexString()
+  } else {
+    if (slicer.controller != "") {
+      // TODO: Check condition is correct
+      slicer.royaltyReceiver = slicer.controller
+    } else {
+      slicer.royaltyReceiver = slicer.creator
+    }
+  }
+
+  slicer.save()
+}
 
 export function handleTransferSingle(event: TransferSingleEvent): void {
-  let fromAddress = event.params.from.toHexString()
+  let from = event.params.from.toHexString()
+  let to = event.params.to.toHexString()
+  let slicerId = event.params.id.toHex()
+  let value = event.params.value
   let address0 = new Bytes(20) as Address
-  if (fromAddress != address0.toHexString()) {
-    let toAddress = event.params.to.toHexString()
-    let slicerId = event.params.id.toString()
-    let value = event.params.value
 
-    let to = Payee.load(toAddress)
-    let fromSlicer = PayeeSlicer.load(slicerId + "-" + fromAddress)!
-    let toSlicer = PayeeSlicer.load(slicerId + "-" + toAddress)
-    if (!to) {
-      to = new Payee(toAddress)
-      to.save()
+  if (from != address0.toHexString() && from != to) {
+    let toPayee = Payee.load(to)
+    let fromSlicer = PayeeSlicer.load(from + "-" + slicerId)!
+    let toSlicer = PayeeSlicer.load(to + "-" + slicerId)
+    if (!toPayee) {
+      toPayee = new Payee(to)
+      toPayee.save()
     }
     if (!toSlicer) {
-      toSlicer = new PayeeSlicer(slicerId + "-" + toAddress)
-      toSlicer.payee = toAddress
+      toSlicer = new PayeeSlicer(to + "-" + slicerId)
+      toSlicer.payee = to
       toSlicer.slicer = slicerId
-      toSlicer.slices = BigInt.fromI32(0)
+      // toSlicer.slices = BigInt.fromI32(0)
     }
-    // TODO: Add check when sender and receiver are the same person
     toSlicer.slices = toSlicer.slices.plus(value)
     fromSlicer.slices = fromSlicer.slices.minus(value)
     fromSlicer.save()
@@ -35,28 +217,28 @@ export function handleTransferSingle(event: TransferSingleEvent): void {
 }
 
 export function handleTransferBatch(event: TransferBatchEvent): void {
-  let fromAddress = event.params.from.toHexString()
+  let from = event.params.from.toHexString()
+  let to = event.params.to.toHexString()
+  let ids = event.params.ids
+  let values = event.params.values
   let address0 = new Bytes(20) as Address
-  if (fromAddress != address0.toHexString()) {
-    let toAddress = event.params.to.toHexString()
-    let to = Payee.load(toAddress)
-    if (!to) {
-      to = new Payee(toAddress)
-      to.save()
+  if (from != address0.toHexString() && from != to) {
+    let toPayee = Payee.load(to)
+    if (!toPayee) {
+      toPayee = new Payee(to)
+      toPayee.save()
     }
-    let ids = event.params.ids
-    let values = event.params.values
     for (let i = 0; i < ids.length; i++) {
-      let slicerId = ids[i].toString()
+      let slicerId = ids[i].toHex()
       let value = values[i]
 
-      let fromSlicer = PayeeSlicer.load(slicerId + "-" + fromAddress)!
-      let toSlicer = PayeeSlicer.load(slicerId + "-" + toAddress)
+      let fromSlicer = PayeeSlicer.load(from + "-" + slicerId)!
+      let toSlicer = PayeeSlicer.load(to + "-" + slicerId)
       if (!toSlicer) {
-        toSlicer = new PayeeSlicer(slicerId + "-" + toAddress)
-        toSlicer.payee = toAddress
+        toSlicer = new PayeeSlicer(to + "-" + slicerId)
+        toSlicer.payee = to
         toSlicer.slicer = slicerId
-        toSlicer.slices = BigInt.fromI32(0)
+        // toSlicer.slices = BigInt.fromI32(0)
       }
       toSlicer.slices = toSlicer.slices.plus(value)
       fromSlicer.slices = fromSlicer.slices.minus(value)
--- src/mappings/slicer.ts
+++ src/mappings/slicer.ts
@@ -1,202 +1,137 @@
 import {
   Slicer as SlicerEntity,
-  PayeeSlicer,
-  Product,
-  ProductPurchase,
   Payee,
+  Currency,
+  PayeeSlicer,
+  CurrencySlicer,
   TokenReceived
 } from "../../generated/schema"
 import {
+  Released as ReleasedEvent,
+  CurrenciesAdded as CurrenciesAddedEvent,
+  ChildSlicerSet as ChildSlicerSetEvent,
+  CustomFeeSet as CustomFeeSetEvent,
   PaymentReceived as PaymentReceivedEvent,
-  TriggeredRelease as TriggeredSlicerReleaseEvent,
-  AddedChildrenSlicer as AddedChildrenSlicerEvent,
-  ProductAdded as ProductAddedEvent,
-  ProductInfoChanged as ProductInfoChangedEvent,
-  ProductCurrencyChanged as ProductCurrencyChangedEvent,
-  ProductCategoryChanged as ProductCategoryChangedEvent,
-  ProductRemoved as ProductRemovedEvent,
-  ProductPaid as ProductPaidEvent,
   ERC721Received as ERC721ReceivedEvent,
   ERC1155Received as ERC1155ReceivedEvent,
   ERC1155BatchReceived as ERC1155BatchReceivedEvent
 } from "../../generated/templates/Slicer/Slicer"
-import { BigInt, Bytes, dataSource } from "@graphprotocol/graph-ts"
+import { BigInt, dataSource } from "@graphprotocol/graph-ts"
+
+export function handleReleased(event: ReleasedEvent): void {
+  let context = dataSource.context()
+  let slicerId = context.getString("slicerId")
+  let currency = event.params.currency.toHexString()
+  let amountReleased = event.params.amountReleased
+  let protocolPayment = event.params.protocolPayment
+
+  let currencySlicer = CurrencySlicer.load(currency + "-" + slicerId)!
+  currencySlicer.released = currencySlicer.released.plus(amountReleased)
+  currencySlicer.releasedToProtocol = currencySlicer.releasedToProtocol.plus(
+    protocolPayment
+  )
+
+  currencySlicer.save()
+}
+
+export function handleCurrenciesAdded(event: CurrenciesAddedEvent): void {
+  let context = dataSource.context()
+  let slicerId = context.getString("slicerId")
+  let slicer = SlicerEntity.load(slicerId)!
+  let currencies = event.params.currencies
+
+  for (let i = 0; i < currencies.length; i++) {
+    let currencyAddress = currencies[i].toHexString()
+    if (!slicer.currencies.includes(currencyAddress)) {
+      let currency = Currency.load(currencyAddress)
+      if (!currency) {
+        currency = new Currency(currencyAddress)
+        currency.save()
+      }
+
+      let currencySlicer = new CurrencySlicer(currencyAddress + "-" + slicerId)
+      currencySlicer.currency = currencyAddress
+      currencySlicer.slicer = slicerId
+      currencySlicer.save()
+    }
+  }
+
+  slicer.save()
+}
+
+// Todo: Check if it works
+export function handleChildSlicerSet(event: ChildSlicerSetEvent): void {
+  let context = dataSource.context()
+  let slicerId = context.getString("slicerId")
+  let slicer = SlicerEntity.load(slicerId)!
+  let childSlicerId = event.params.slicerId.toHex()
+  let isAdded = event.params.addChildSlicerMode
+  let childrenSlicers = slicer.childrenSlicers
+  if (isAdded) {
+    slicer.childrenSlicers.push(childSlicerId)
+  } else {
+    let index = -1
+    for (let i = 0; i < childrenSlicers.length; i++) {
+      if (childrenSlicers[i] == childSlicerId) {
+        index = i
+      }
+    }
+
+    if (index != -1) {
+      slicer.childrenSlicers.splice(index, 1)
+    }
+  }
+  slicer.save()
+}
+
+export function handleCustomFeeSet(event: CustomFeeSetEvent): void {
+  let context = dataSource.context()
+  let slicerId = context.getString("slicerId")
+  let slicer = SlicerEntity.load(slicerId)!
+  let customFeeActive = event.params.customFeeActive
+  let customFee = event.params.customFee
+
+  if (customFeeActive) {
+    slicer.protocolFee = customFee
+  } else {
+    slicer.protocolFee = BigInt.fromI32(25)
+  }
+
+  slicer.save()
+}
 
 export function handlePaymentReceived(event: PaymentReceivedEvent): void {
   let context = dataSource.context()
   let slicerId = context.getString("slicerId")
   let slicer = SlicerEntity.load(slicerId)!
-  let buyerAddress = event.params.from.toHexString()
+  let sender = event.params.from.toHexString()
+  let amount = event.params.amount
 
-  let payee = Payee.load(buyerAddress)
+  let payee = Payee.load(sender)
   if (!payee) {
-    payee = new Payee(buyerAddress)
+    payee = new Payee(sender)
     payee.save()
   }
 
-  let payeeSlicer = PayeeSlicer.load(slicerId + "-" + buyerAddress)
+  let payeeSlicer = PayeeSlicer.load(sender + "-" + slicerId)
   if (!payeeSlicer) {
-    payeeSlicer = new PayeeSlicer(slicerId + "-" + buyerAddress)
-    payeeSlicer.payee = buyerAddress
+    payeeSlicer = new PayeeSlicer(sender + "-" + slicerId)
+    payeeSlicer.payee = sender
     payeeSlicer.slicer = slicerId
     payeeSlicer.slices = BigInt.fromI32(0)
   }
-  slicer.totalReceived = slicer.totalReceived.plus(event.params.amount)
-  payeeSlicer.totalPaid = payeeSlicer.totalPaid!.plus(event.params.amount)
+
+  slicer.ethReceived = slicer.ethReceived.plus(amount)
+  payeeSlicer.ethSent = payeeSlicer.ethSent.plus(amount)
   payeeSlicer.save()
   slicer.save()
-}
-
-export function handleTriggeredSlicerRelease(
-  event: TriggeredSlicerReleaseEvent
-): void {
-  let payeeAddress = event.params.payee.toHexString()
-  let context = dataSource.context()
-  let slicerId = context.getString("slicerId")
-  let ps = PayeeSlicer.load(slicerId + "-" + payeeAddress)!
-  ps.released = ps.released!.plus(event.params.released)
-  ps.save()
-}
-
-// Todo: fix
-export function handleAddedChildrenSlicer(
-  event: AddedChildrenSlicerEvent
-): void {
-  let context = dataSource.context()
-  let slicerId = context.getString("slicerId")
-  let slicer = SlicerEntity.load(slicerId)!
-  slicer.childrenSlicers!.push(event.params.slicerId.toString())
-  slicer.save()
-}
-
-export function handleProductAdded(event: ProductAddedEvent): void {
-  let context = dataSource.context()
-  let slicerId = context.getString("slicerId")
-  let product = new Product(slicerId + "-" + event.params.productId.toString())
-  let subSlicers = event.params.subSlicersId
-  let subProducts = event.params.subProducts
-  product.slicer = slicerId
-  product.categoryIndex = event.params.categoryIndex
-  product.price = event.params.price
-  product.isUSD = event.params.isUSD
-  product.isInfinite = event.params.isInfinite
-  product.isMultiple = event.params.isMultiple
-  product.availableUnits = event.params.availableUnits
-  product.creator = event.params.creator
-  product.data = event.params.data
-  product.totalPurchases = BigInt.fromI32(0)
-  product.createdAtTimestamp = event.block.timestamp
-  product.subProducts = []
-  for (let i = 0; i < subSlicers.length; i++) {
-    product.subProducts.push(
-      subSlicers[i].toString() + "-" + subProducts[i].toString()
-    )
-  }
-  product.save()
-}
-
-export function handleProductInfoChanged(event: ProductInfoChangedEvent): void {
-  let context = dataSource.context()
-  let slicerId = context.getString("slicerId")
-  let product = Product.load(
-    slicerId + "-" + event.params.productId.toString()
-  )!
-  product.isInfinite = event.params.isInfinite
-  product.availableUnits = event.params.units
-  product.price = event.params.productPrice
-  product.save()
-}
-
-export function handleProductCurrencyChanged(
-  event: ProductCurrencyChangedEvent
-): void {
-  let context = dataSource.context()
-  let slicerId = context.getString("slicerId")
-  let product = Product.load(
-    slicerId + "-" + event.params.productId.toString()
-  )!
-  product.isUSD = event.params.isUSD
-  product.price = event.params.productPrice
-  product.save()
-}
-
-export function handleProductCategoryChanged(
-  event: ProductCategoryChangedEvent
-): void {
-  let context = dataSource.context()
-  let slicerId = context.getString("slicerId")
-  let product = Product.load(
-    slicerId + "-" + event.params.productId.toString()
-  )!
-  product.categoryIndex = event.params.categoryIndex
-  product.save()
-}
-
-export function handleProductRemoved(event: ProductRemovedEvent): void {
-  let context = dataSource.context()
-  let slicerId = context.getString("slicerId")
-  let product = Product.load(
-    slicerId + "-" + event.params.productId.toString()
-  )!
-  product.availableUnits = BigInt.fromI32(0)
-  product.categoryIndex = BigInt.fromI32(0)
-  product.data = new Bytes(0)
-  product.isInfinite = false
-  product.isMultiple = false
-  product.isUSD = false
-  product.price = BigInt.fromI32(0)
-  product.save()
-}
-
-export function handleProductPaid(event: ProductPaidEvent): void {
-  let context = dataSource.context()
-  let slicerId = context.getString("slicerId")
-  let productId = slicerId + "-" + event.params.productId.toString()
-  let buyerAddress = event.params.from.toHexString()
-  let quantity = BigInt.fromI32(event.params.quantity)
-
-  let product = Product.load(productId)!
-  product.totalPurchases = product.totalPurchases.plus(quantity)
-  product.availableUnits = product.availableUnits.minus(quantity)
-
-  let payee = Payee.load(buyerAddress)
-  if (!payee) {
-    payee = new Payee(buyerAddress)
-    payee.save()
-  }
-  let pp = ProductPurchase.load(productId + "-" + buyerAddress)
-  if (!pp) {
-    pp = new ProductPurchase(productId + "-" + buyerAddress)
-    pp.product = productId
-    pp.buyerSlicer = slicerId + "-" + buyerAddress
-    pp.buyer = buyerAddress
-    // pp.hash = []
-    pp.quantity = BigInt.fromI32(0)
-  }
-
-  let payeeSlicer = PayeeSlicer.load(slicerId + "-" + buyerAddress)
-  if (!payeeSlicer) {
-    payeeSlicer = new PayeeSlicer(slicerId + "-" + buyerAddress)
-    payeeSlicer.payee = buyerAddress
-    payeeSlicer.slicer = slicerId
-    payeeSlicer.slices = BigInt.fromI32(0)
-  }
-  payeeSlicer.totalPaidProducts = payeeSlicer.totalPaidProducts!.plus(
-    event.params.productPrice
-  )
-  // pp.hash.push(event.transaction.hash)
-  pp.quantity = pp.quantity.plus(quantity)
-  pp.lastPurchasedAtTimestamp = event.block.timestamp
-  product.save()
-  pp.save()
-  payeeSlicer.save()
 }
 
 export function handleERC721Received(event: ERC721ReceivedEvent): void {
   let context = dataSource.context()
   let slicerId = context.getString("slicerId")
   let contract = event.params.contractAddress.toHexString()
-  let tokenId = event.params.tokenId.toString()
+  let tokenId = event.params.tokenId.toHex()
 
   let tokenReceived = TokenReceived.load(
     slicerId + "-" + contract + "-" + tokenId
@@ -217,7 +152,7 @@ export function handleERC1155Received(event: ERC1155ReceivedEvent): void {
   let context = dataSource.context()
   let slicerId = context.getString("slicerId")
   let contract = event.params.contractAddress.toHexString()
-  let tokenId = event.params.tokenId.toString()
+  let tokenId = event.params.tokenId.toHex()
   let amount = event.params.amount
 
   let tokenReceived = TokenReceived.load(
@@ -229,13 +164,13 @@ export function handleERC1155Received(event: ERC1155ReceivedEvent): void {
     tokenReceived.contract = event.params.contractAddress
     tokenReceived.tokenId = event.params.tokenId
     tokenReceived.quantity = amount
-    tokenReceived.isERC721 = false
   } else {
     tokenReceived.quantity = tokenReceived.quantity.plus(amount)
   }
   tokenReceived.lastReceivedAtTimestamp = event.block.timestamp
   tokenReceived.save()
 }
+
 export function handleERC1155BatchReceived(
   event: ERC1155BatchReceivedEvent
 ): void {
@@ -246,7 +181,7 @@ export function handleERC1155BatchReceived(
   let amounts = event.params.amounts
 
   for (let i = 0; i < tokenIds.length; i++) {
-    let tokenId = tokenIds[i].toString()
+    let tokenId = tokenIds[i].toHex()
     let amount = amounts[i]
 
     let tokenReceived = TokenReceived.load(
@@ -260,7 +195,6 @@ export function handleERC1155BatchReceived(
       tokenReceived.contract = event.params.contractAddress
       tokenReceived.tokenId = tokenIds[i]
       tokenReceived.quantity = amount
-      tokenReceived.isERC721 = false
     } else {
       tokenReceived.quantity = tokenReceived.quantity.plus(amount)
     }
@@ -268,13 +202,3 @@ export function handleERC1155BatchReceived(
     tokenReceived.save()
   }
 }
-
-// export function handleSLCPaid(event: SLCPaidEvent): void {
-//   let entity = new SLCPaid(
-//     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-//   )
-//   entity.serviceIds = event.params.serviceIds
-//   entity.quantities = event.params.quantities
-//   entity.amount = event.params.amount
-//   entity.save()
-// }
